e3a3cb4c6b3ccc384745c1f2724774278c3dec84
devallmeida <carlosplalmeida@gmail.com>
2025-08-05 17:28:59 -0300

    feat: Adiciona primeiro programa 'Ola, Mundo!'

---
+++ primeiroprograma.java
@@ -0,0 +1,8 @@
+package PrimeiroPrograma;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo!");
+    }
+    }
+// O meu primeiro programa em Java
